8fb6c54c370f8de89872ae4d7642a74b1a3c4477
Gabriel Hernan Osores <96618992+Gabo1618@users.noreply.github.com>
2022-11-11 11:51:10 -0300

    Java Leccion 1
    Java UTN Leccion 1

---
+++ Leccion1/HolaMundo/src/pasoporreferencia/PasoPorReferencia.java
@@ -0,0 +1,12 @@
+//Paso por referencia
+package pasoporreferencia;
+
+public class PasoPorReferencia {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static void cambiarValor(){
+        
+    }
+}
